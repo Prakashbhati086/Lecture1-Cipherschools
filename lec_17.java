2f8bf5ea1f8ad0c58eb23fd0001c7c6a6130e8f2
PRAKASH BHATI <122536126+Prakashbhati086@users.noreply.github.com>
2023-07-15 22:17:50 +0530

    Update lec_17.java

--- lec_17.java
+++ lec_17.java
@@ -28,3 +28,22 @@ public class jagged_arrays {
         print_jaggu(arr2);
     }
 }
+//scanner
+
+import java.util.Scanner;
+public class scanner_class {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String f_name=sc.next();// will take input untill the next whitespace
+        String l_name=sc.next();
+        System.out.println("Enter your profession : ");
+        String prof=sc.next();
+        sc.nextLine();
+        System.out.println("Enter your Address : ");
+        String pata=sc.nextLine();
+        System.out.println("Name : "+f_name+""+ l_name);
+        System.out.println("Profession : "+prof);
+        System.out.println("Address : "+pata);
+    }
+}
